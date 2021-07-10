5aa714d8588c05769851d1e1afb236a3ed1eb707
user <user@DESKTOP-V482O0D>
2021-07-10 02:51:05 +0200

    TRABAJO EN CLASE

--- src/herenciasentencia/Main3.java
+++ src/herenciasentencia/Main3.java
@@ -2,9 +2,14 @@ package herenciasentencia;
 
 public class Main3 {
 
+	private static final int N = 0;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("TABLA  7");
+		int n = 7;
+		for (int i = 0; i <= 12; i++)
+			System.out.println(i + "*" + n + "=" + i * n);
 
 	}
-
 }
