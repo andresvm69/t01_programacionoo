88962a062b4552769d6310f38e313cc0f37e4858
diego <diego@LAPTOP-7QUSVIB8>
2021-07-16 19:20:30 -0500

    Taller 5

--- src/MinFigura2.java
+++ src/MinFigura2.java
@@ -4,13 +4,14 @@ public class MinFigura2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//Buen trabajo "Figura inversa"
+		//
 		System.out.println("Ingrese el numero de filas");
 		Scanner teclado = new Scanner(System.in);
 
 		int n = teclado.nextInt();
-	
-		
-		for (int fila = 1; fila <= n ;  fila=fila + 1) {
+
+		for (int fila = 1; fila <= n; fila = fila + 1) {
 
 			for (int ast = n; ast >= fila; ast = ast - 1) {
 				System.out.print("*");
