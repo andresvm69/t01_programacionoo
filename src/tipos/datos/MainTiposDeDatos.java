3915ad5982f59eabe24a9c87289d4b66560ed201
user <user@DESKTOP-V482O0D>
2021-07-22 01:50:07 +0200

    22/7/2021 Taller

---
+++ src/tipos/datos/MainTiposDeDatos.java
@@ -0,0 +1,19 @@
+package tipos.datos;
+
+public class MainTiposDeDatos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		short cantidaEstudiantes = 100;
+		byte entradaCircuito = 0;
+		int cantCiudadanos = 100000;
+		long cantMundo = 1000000000;
+		float sueldo = (float) 10.56;
+		//float sueldo = 10.56(foF);
+		double saldoBanco = 10000000.67;
+		char opcionMenu = 'C';
+		//no usar carecteres especiales
+		boolean casado = true;
+	}
+
+}
