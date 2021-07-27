e3f9267ba04467f38d70305f44ae9a2df2b3335b
user <user@DESKTOP-V482O0D>
2021-07-27 03:05:03 +0200

    PRUEBA

---
+++ src/julio/andres/velastegui/medina/Main.java
@@ -0,0 +1,46 @@
+package julio.andres.velastegui.medina;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ingrese numero");
+		
+		Scanner teclado = new Scanner(System.in);// TECLADO
+
+		int N = teclado.nextInt();
+		int N1 = teclado.nextInt();
+		int N2 = teclado.nextInt();
+
+		
+		if(N <= -1){ 
+			//SI
+			System.out.println("numero" + N);
+		}else {
+			//NO
+			System.out.println("Ingrese numero");
+		
+		}
+		if(N1 <= -1){ 
+			//SI
+			System.out.println("numero" + N1);
+		}else {
+			//NO
+			System.out.println("Ingrese numero");
+		
+		}
+		
+		if(N2 <= -1){ 
+			//SI
+			System.out.println("numero" + N2);
+		}else {
+			//NO
+			System.out.println("Ingrese numero");
+
+
+	}
+	}
+}
+
