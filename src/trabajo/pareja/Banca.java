80408fc7ec111b1a5bdab53bb6f6e9a91638683d
user <user@DESKTOP-V482O0D>
2021-08-13 20:44:33 +0200

    TRABAJO EN PAREJAS

---
+++ src/trabajo/pareja/Banca.java
@@ -0,0 +1,22 @@
+package trabajo.pareja;
+
+public class Banca {
+	
+	public String web;
+	public String movil;
+	
+	public Banca () {
+	
+	}
+		public void validar (String datosAimprimir, int letra) {
+			System.out.println("Banco Pichincha");
+			System.out.println("************");
+			System.out.println("Banca movil");
+			System.out.println("************");
+			System.out.println(datosAimprimir);
+			System.out.println("************");
+			System.out.println("Confianza en nuestras manos");
+		
+	}
+
+}
---
+++ src/trabajo/pareja/Compraador.java
@@ -0,0 +1,15 @@
+package trabajo.pareja;
+
+public class Compraador {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ConcesionariaAutos acreditar1 = new ConcesionariaAutos ();
+		acreditar1.acreditar("Credito aceptado" , 10);
+		
+
+		
+
+	}
+
+}
